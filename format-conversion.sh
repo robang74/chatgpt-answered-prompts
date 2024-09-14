b388cf09fe0ad3c4687a64e4a291e9ee6feb0687
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2024-09-14 19:11:35 +0200

    format-conversion.sh bash script created new
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

---
+++ format-conversion.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -e
+#
+# (C) Roberto A. Foglietta <roberto.foglietta@gmail.com> - 3-clause BSD
+#
+
+echo
+
+for i in *.md; do
+    echo "converting $i in html ..."
+    markdown $i > html/${i%.md}.html
+done
+
+echo
+echo "redirecting html links ..."
+
+for i in *.png; do
+    for j in html/*.html; do
+        sed -i "s,\(href=\"\)$i,\\1../${i%.md},g" $j
+    done
+done
+
+for i in *.md; do
+    for j in html/*.html; do
+        sed -i "s,\(href=\"\)$i\">$i,\\1${i%.md}.html\">${i%.md}.html,g" $j
+    done
+done
+
+echo
+for i in *.md; do
+    echo "converting $i in pdf ..."
+    cp -f $i pdf/$i.tmp
+    for k in *.png *.md; do
+        sed -i "s,\[.*\]($k),$k,g" pdf/$i.tmp
+    done
+    md2pdf pdf/$i.tmp pdf/${i%.md}.pdf
+    rm -f pdf/$i.tmp
+done
+
+echo
+echo "all done."
+echo
+exit
+
+
+
+echo
+echo "redirecting pdf link ..."
+
+for i in *.png; do
+    for j in pdf/*.pdf; do
+        echo "reworking $j ..."
+        rm -f $j.tmp
+        pdftk $j output $j.tmp uncompress
+        sed -i "s,\(/URI (file://\).*/$i,\\1../$i,g" $j.tmp
+        pdftk $j.tmp output $j compress
+        rm -f $j.tmp
+    done
+done
+
+
+
+
+
+
