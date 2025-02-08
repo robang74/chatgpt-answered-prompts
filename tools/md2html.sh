747d962150a6a5f7571f38651b5bb6b013a72ecd
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2025-02-08 01:54:46 +0100

    tools/md2html.sh: bug fix
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- tools/md2html.sh
+++ tools/md2html.sh
@@ -119,7 +119,7 @@ function md2htmlfunc() {
 
     sed -i $2 -e "s,^>$,> ," -e "s,@,\&commat;,g" \
 -e 's,\\\*,\&ast;,g' -e 's,(\*),(\&ast;),g' -e 's,\[\*\],[\&ast;],g' \
--e 's,[^!/]-->,\&rarr;,g' -e 's,<--,\&larr;,g' \
+-e 's,\([^!/]\)-->,\1\&rarr;,g' -e 's,<--,\&larr;,g' \
 -e 's,\(>\)\{0\,1\} -- ,\1 \&mdash; ,g' \
 -e "s,>  *\[\!WARN\],> $warn_A," -e "s,>  *\[\!WARNING\],> $warn_A," \
 -e "s,>  *\[\!NOTE\],> $note_A," -e "s,>  *\[\!NOTICE\],> $note_A," \
